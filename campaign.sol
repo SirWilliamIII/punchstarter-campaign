9b6f100faad55c7a2a8e2fabc28700139df8f944
Will Carpenter <willcarpenteriii@gmail.com>
2018-02-27 16:42:08 -0600

    transfer funds once 51% of approvers approve of request

--- campaign.sol
+++ campaign.sol
@@ -50,4 +50,12 @@ contract Campaign {
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
 }
